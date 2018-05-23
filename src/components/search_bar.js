7c1ff9fc362af3f2d815d5817179fafd9071fda3
Lisa Trevis <l_trevis@hotmail.com>
2018-05-22 22:37:53 -0700

    first component

---
+++ src/components/search_bar.js
@@ -0,0 +1,49 @@
+// import React from 'react';
+// Refactored with ES6:
+import React, { Component } from 'react';
+
+// This is a functional component, named because it is a literal function:
+// const SearchBar = () => {
+// 	return <input />; // Same as React.createElement call, so need to import React
+// };
+
+// But what I want is a class component, so I'll declare a new class named SearchBar and give it all of the functionality of the React.Component class. Every component that is class based must have a render method and return some JSX (or it will throw an error):
+// class SearchBar extends React.Component {
+// 	render() {
+// 		return <input />;
+// 	}
+// }
+// I can declare a new instance of the SearchBar class with new SearchBar
+
+// Refactored with ES6:
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: ''};
+	}
+
+	render() {
+		// Pass event handler into the element that we want to watch for the event, and declare the event handler to trigger whenever the input box content is changed by the user::
+		return (
+			<div>
+				<input // A controlled component because it's receiving it's value from state:
+				value={this.state.term}
+				onChange={event => this.setState({ term: event.target.value })} />
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
+
+
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+// Imported libraries do not need a relative file path:
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Files I've created myself DO need a relative file path:
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDC0O0NP3IaD2dQesQWt-7J6IBVqjlQZH0';
+
 // This is the App class, which is like a factory and returns some JSX:
 const App = () => {
-	return <div>Hi!</div>;
+	// Multiline JSX is usually written inside of parens:
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 // This instance of App takes the generated HTML and renders it to the DOM:
